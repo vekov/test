b4677b33b4a5cf6ee5717aafbaffdd915d278c48
vekov <vekov@users.noreply.github.com>
2018-04-04 11:52:33 +0300

    Create test.scala

---
+++ test.scala
@@ -0,0 +1,13 @@
+
+object Test extends App {
+  if (args.length == 0) {
+    println("add some parameters")
+  }
+
+  def compare(str1: String, str2: String): Boolean = {
+    str1.length == str2.length
+  }
+
+  println(compare(args(0), args(1)))
+
+}
