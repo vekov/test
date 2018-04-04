450323805c02e6f55222c7f1a702fd622aa9ff93
Ivailo Vekov <ivailo.vekov@qvantel.com>
2018-04-04 16:40:38 +0300

    fpaa

--- fpaa_1.scala
+++ fpaa_1.scala
@@ -1,1 +1,16 @@
 // fpaa_1.scala
+
+val arr = Array("this", "is", "some", "kind", "of", "array")
+
+val thread = new Thread {
+  override def run {
+    printStringLength(arr)
+  }
+}
+thread.start
+
+//arr(4) = null
+
+def printStringLength(xs: Seq[String]) {
+  for (x <- xs) println(x.length)
+}
